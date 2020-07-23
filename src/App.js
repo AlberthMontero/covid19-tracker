572f6dac1c0795e83d17490a88ab24499030a3de
Alberth Montero <alberthm@gmail.com>
2020-07-23 21:43:32 +0200

    Added header with material UI

--- src/App.js
+++ src/App.js
@@ -1,8 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { FormControl, Select, MenuItem } from '@material-ui/core';
 import './App.css';
 
 function App() {
-  return <div className="app"></div>;
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState('DO');
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch('https://disease.sh/v3/covid-19/countries')
+        .then(response => response.json())
+        .then(data => {
+          const countries = data.map(country => ({
+            name: country.country,
+            value: country.countryInfo.iso2
+          }));
+          setCountries(countries);
+        })
+        .catch(err => {
+          console.error(err);
+        });
+    };
+
+    getCountriesData();
+  }, []);
+
+  const onCountryChange = event => {
+    const countryCode = event.target.value;
+
+    setCountry(countryCode);
+  };
+
+  return (
+    <div className="app">
+      <div className="app__header">
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" onChange={onCountryChange} value={country}>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            {countries.map((country, idx) => (
+              <MenuItem key={idx} value={country.value}>
+                {country.name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+
+      {/* InfoBoxes */}
+      {/* InfoBoxes */}
+      {/* InfoBoxes */}
+
+      {/* Table */}
+      {/* Graph */}
+
+      {/* Map */}
+    </div>
+  );
 }
 
 export default App;
