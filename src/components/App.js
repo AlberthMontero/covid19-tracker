37605271b66b5a23559df8c5e9f57a6ad9c20169
Alberth Montero <alberthm@gmail.com>
2020-07-24 02:28:35 +0200

    Added Table, LineGraph and Map

--- src/components/App.js
+++ src/components/App.js
@@ -6,20 +6,24 @@ import {
   Card,
   CardContent
 } from '@material-ui/core';
-import InfoBox from './InfoBox';
 import Map from './Map';
 import Table from './Table';
+import InfoBox from './InfoBox';
+import LineGraph from './LineGraph';
 import { sortData } from '../utils';
+import 'leaflet/dist/leaflet.css';
 import './App.css';
 
 function App() {
   const [countries, setCountries] = useState([]);
-  const [country, setCountry] = useState('DO');
+  const [country, setCountry] = useState('worldwide');
   const [countryInfo, setCountryInfo] = useState({});
   const [tableData, setTableData] = useState([]);
+  const [mapCenter, setMapCenter] = useState({ lat: 34.80746, lng: -40.4796 });
+  const [mapZoom, setMapZoom] = useState(3);
 
   useEffect(() => {
-    fetch('https://disease.sh/v3/covid-19/countries/DO')
+    fetch('https://disease.sh/v3/covid-19/all')
       .then(response => response.json())
       .then(data => {
         setCountryInfo(data);
@@ -104,15 +108,14 @@ function App() {
           />
         </div>
 
-        <Map />
+        <Map center={mapCenter} zoom={mapZoom} />
       </div>
       <Card className="app__right">
         <CardContent>
           <h3>Live Cases by Country</h3>
           <Table countries={tableData} />
           <h3>Worldwide New Cases</h3>
-          {/* Table */}
-          {/* Graph */}
+          <LineGraph />
         </CardContent>
       </Card>
     </div>
